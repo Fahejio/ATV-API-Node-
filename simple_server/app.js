00bbb0a84a4388c840a6b09aa5d08f79d4a2deb3
Fahejio <rafaelfernandes816@gmail.com>
2023-05-14 23:25:28 -0300

    Upload

---
+++ simple_server/app.js
@@ -0,0 +1,27 @@
+const express = require('express'),
+path = require('path');
+const bodyParser = require('body-parser'),
+cors = require('cors'),
+mongoose = require('mongoose');
+
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://127.0.0.1:27017/mean', { useNewUrlParser: true, useUnifiedTopology: true }).then(
+  () => {console.log('Database is connected') },
+  err => { console.log('Can not connect to the database'+ err)});
+
+const userRoute = require('./routes/user.route');
+const industryRoute = require('./routes/industry.route');
+var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+app.use('/user', userRoute);
+app.use('/industry', industryRoute);
+app.get('/', function(req, res){
+   res.send("Hello World!");
+});
+app.listen(3000,function(){
+    console.log('Listening on port 3000!');
+});
